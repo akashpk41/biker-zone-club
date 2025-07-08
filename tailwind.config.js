8bea150a9acd7d68e5d172bdc21ff8cd5e0b504f
akashpk41 <akashpk84100@gmail.com>
2025-07-08 21:18:47 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // adjust paths based on your project
+  ],
+  theme: {
+    extend: {
+      colors: {
+       "bike-btn-primary": '#da373d', // Add your custom color
+      },
+    },
+  },
+  plugins: [],
+}
